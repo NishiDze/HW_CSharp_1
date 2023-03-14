2749317af9a20a115ae70c037f75705eaccadef9
Lybov <nishixkonnor@mail.ru>
2023-03-14 19:19:44 +0300

    Add HW

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write ("Введите 2 числа ");
+// int num = int.Parse (Console.ReadLine()!);
+// int num1 = int.Parse (Console.ReadLine()!);
+// if (num > num1)
+// {
+//     System.Console.WriteLine($"Число {num} больше");
+// }
+// else
+// {
+//         System.Console.WriteLine($"Число {num1} больше");
+
+// }
+
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.Write ("Введите три числа: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// int num2 = int.Parse(Console.ReadLine()!);
+// int num3 = int.Parse(Console.ReadLine()!);
+// int max = num1; //4 2 3
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+// Console.Write ($"Максимум это {max} ");
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка(number%2==0)).
+// Console.Write ("Введите число ");
+// int num = int.Parse (Console.ReadLine()!);
+// if (num%2==0)
+// {
+//     System.Console.WriteLine("Четное");
+// }
+// else 
+// System.Console.WriteLine("Нечетное");
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.Write ("Введите число ");
+// int num = int.Parse (Console.ReadLine()!);
+// int i = 2;
+// // while (i<=num)
+// // {
+// //   if (i%2==0)
+// //   {
+// //     System.Console.WriteLine(i);
+// //   } 
+// //   i++;
+// // }
+// while (i<=num)
+// {
+// System.Console.WriteLine(i);
+// i = i + 2;
+// }
